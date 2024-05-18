7094eee687b9d4ee47654cc946d452d0c05d2c93
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-05-18 22:45:13 +0530

    mongodb database configure

---
+++ src/Controllers/authController.ts
@@ -0,0 +1,10 @@
+
+export const register = async (req: Request, res: Response) => {
+    try {
+
+    }catch (error) {}
+}
+
+export const login = async (req: Request, res: Response) => {
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("salman");
+import mongoose from "mongoose";
+import express from 'express'
+import  process from "process";
+import bodyParser from "body-parser";
+
+
+var cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+const dotenv=require('dotenv')
+dotenv.config({ path: 'src/.env' });
+
+const app=express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+let port=process.env.SERVER
+
+mongoose.connect('mongodb://127.0.0.1:27018/doctor_appointment').then(()=>{
+    app.listen(port,()=>{
+        console.log(`serve running ${8080}`)
+
+    })
+})
