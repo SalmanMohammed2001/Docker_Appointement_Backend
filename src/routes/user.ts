221bb242dd4cd4f51658fea820dc36efde4a37ca
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-05-24 22:59:22 +0530

    implementation getAllMyAppointments getUserProfile routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,12 @@
 import express from "express";
-import {updateUser,deleteUser,getAllUser,getSingleUser} from "../Controllers/userController";
+import {
+    updateUser,
+    deleteUser,
+    getAllUser,
+    getSingleUser,
+    getUserProfile,
+    getAllMyAppointments
+} from "../Controllers/userController";
 import {restrict, verifyToken} from '../middleware/verifyToken'
 
 const router = express.Router();
@@ -8,5 +15,7 @@ router.get("/find/:id",verifyToken,restrict(["patient"]),getSingleUser)
 router.get("/findAll",verifyToken,restrict(["admin"]),getAllUser)
 router.put("/update/:id",verifyToken,restrict(["patient"]),updateUser)
 router.delete("/delete/:id",verifyToken,restrict(["patient"]),deleteUser)
+router.delete("/profile/me",verifyToken,restrict(["patient"]),getUserProfile)
+router.delete("/appointments/my-appointments",verifyToken,restrict(["patient"]),getAllMyAppointments)
 
 module.exports=router
